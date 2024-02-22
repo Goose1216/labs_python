6291bac18858c044a73b3cbefc5d30bedd568266
Goose1216 <goose1216@yandex.ru>
2024-02-22 12:43:17 +0300

    Lab1_Task8_Create

---
+++ 1/task_8.py
@@ -0,0 +1,13 @@
+def cnt_numbers(string):
+    cnt_num = 0
+    cnt_num_max = 0
+    for letter in string:
+        if letter in "0123456789":
+            cnt_num += 1
+        else:
+            cnt_num_max = max(cnt_num_max, cnt_num)
+            cnt_num = 0
+    return cnt_num_max
+
+
+print(cnt_numbers(input()))
