664c5696fec316528f507d9987f97850f04173ad
Goose1216 <goose1216@yandex.ru>
2024-02-22 10:27:32 +0300

    Lab1_Task4

---
+++ 1/task_4.py
@@ -0,0 +1,9 @@
+import re
+
+
+def find_dates(string):
+    answer_group = re.findall(r"([0-2]\d|3[0-1])\.(0\d|1[0-2])\.(\d{4})", string)
+    return tuple(map(lambda x: '.'.join(x), answer_group))
+
+
+print(*find_dates(input()), sep=', ')
