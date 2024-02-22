f85ddda2dc4da009ac575423d8672c14a33c796b
Goose1216 <goose1216@yandex.ru>
2024-02-22 17:17:20 +0300

    Lab1_Task11_CntVowels_Create

---
+++ 1/tasks_11-14.py
@@ -0,0 +1,14 @@
+def sort_by_subtraction(lines):
+    """
+    Отсортировать строки в порядке увеличения разницы между количеством согласных и
+    количеством гласных букв в строке.
+    """
+
+    def cnt_vowels(string):
+        cnt = 0
+        alph = set("аиоуыэеёюя")
+        for letter in string:
+            if letter in alph:
+                cnt += 1
+        return cnt
+
