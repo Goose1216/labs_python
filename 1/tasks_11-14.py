c7b10b0e01d318eed3e8be4e4bdce188d76863be
Goose1216 <goose1216@yandex.ru>
2024-03-03 09:59:14 +0300

    Lab1_Task12_sort_by_ascii_mediumOrd_Create

--- 1/tasks_11-14.py
+++ 1/tasks_11-14.py
@@ -1,6 +1,6 @@
 def sort_by_subtraction(lines):
     """
-    Отсортировать строки в порядке увеличения разницы между количеством согласных и
+    1. Отсортировать строки в порядке увеличения разницы между количеством согласных и
     количеством гласных букв в строке.
     """
 
@@ -21,5 +21,16 @@ def sort_by_subtraction(lines):
         return cnt
 
     return sorted(lines, key=lambda x: cnt_consonants(x) - cnt_vowels(x))
+
+
+def sort_by_ascii(lines):
+    """
+    2. В порядке увеличения квадратичного отклонения среднего веса
+    ASCII-кода символа строки от среднего веса ASCII-кода символа первой строки
+    """
+
+    def medium_ord(line):
+        sum_ord = sum(list(map(lambda x: ord(x), line)))
+        return round(sum_ord / len(line), 4)
 
 
