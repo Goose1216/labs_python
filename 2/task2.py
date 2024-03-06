d52a428fdc97e3e0c979516cdb66ac79b6e315e2
Goose1216 <goose1216@yandex.ru>
2024-03-06 13:42:35 +0300

    Lab2_task2_CheckFiles_Create

--- 2/task2.py
+++ 2/task2.py
@@ -7,3 +7,16 @@ def create_files(cnt_files):
         files[file[0]] = "".join(file[1:])
     return files
 
+
+def check_files(cnt_operations, files):
+    operations = {
+        'read': "R",
+        "write": "W",
+        "execute": "X"
+    }
+    for i in range(cnt_operations):
+        operation, file_name = input().split()
+        if operations[operation] in files[file_name]:
+            print("Access denied")
+        else:
+            print("OK")
