16dc61a7c07b1c343e1341178ae95de16ff3a887
Goose1216 <goose1216@yandex.ru>
2024-03-20 11:08:30 +0300

    Обновил функцию first_enum для четвертой лабораторной, прописал функцию для динамического прохода по циклу, закончил программу

--- 4/task.py
+++ 4/task.py
@@ -1,6 +1,7 @@
-#Вариант 7
+# Вариант 7
+import math
 
-file = open('test.txt', 'r', encoding='utf-8')
+file = open('B.txt', 'r', encoding='utf-8')
 read_file = file.readlines()
 count_house, len_road, weight_max, max_len = (int(x) for x in read_file[0].split())
 data = []
@@ -15,30 +16,52 @@ def fill_tuple():
 def first_enum():
     if max_len >= len_road // 2:
         return sum(x[1] for x in data)
-    best_num_house = 0
-    len_left, len_right = 0, 0
+    ind_house_left, ind_house_right = count_house - 1, 1
     num_house, weight_house = data[0]
-    sum_weight = -(weight_house * 2)
-    i = -1
-    len_left_buffer = len_left
-    while len_left_buffer < max_len:
-        sum_weight += data[0 + i + 1]
-        len_left = len_left_buffer
-        len_left_buffer = (num_house - data[0 + i][0])
-        if len_left_buffer < 0:
-            len_left_buffer = (max_len - data[0 + i][0]) + num_house
+    cnt_packets = math.ceil(weight_house / weight_max)
+    i = -2
+    len_left = 0
+    len_left_next = (len_road - data[-1][0] + num_house)
+    while len_left < max_len:
+        ind_house_left -= 1
+        len_left = len_left_next
+        cnt_packets += math.ceil(data[0 + i + 1][1] / weight_max)
+        len_left_next = (len_road - data[0 + i][0]) + num_house
         i -= 1
-    i = 1
-    len_right_buffer = len_right
-    while len_right_buffer < max_len:
-        sum_weight += data[0 + i - 1]
-        len_right = len_right_buffer
-        len_right_buffer = (data[0 + i][0] - num_house)
-        if len_right_buffer < 0:
-            len_right_buffer = (max_len - num_house + data[0 + i][0])
+    i = 2
+    len_right_last = (data[1][0] - num_house)
+    len_right = 0
+    while len_right < max_len:
+        ind_house_right += 1
+        len_right = len_right_last
+        cnt_packets += math.ceil(data[0 + i - 1][1] / weight_max)
+        len_right_last = data[0 + i][0] - num_house
         i += 1
+    return ind_house_left, ind_house_right, cnt_packets
 
 
-def other_enum():
-    pass
+def enum():
+    try:
+        ind_house_left_last, ind_house_right_last, cnt_packets_first = first_enum()
+    except:
+        return first_enum()
+    cnt_packets_min = cnt_packets_first
+    cnt_packets_last = cnt_packets_first
+    num_house_last = data[0][0]
+    num_house_left_last = data[ind_house_left_last][0]
+    cnt_packets_left_last = math.ceil(data[ind_house_left_last][1] / weight_max)
+    num_house_right_last = data[ind_house_right_last][0]
+    cnt_packets_right_last = math.ceil(data[ind_house_right_last][1] / weight_max)
+    for ind_house_new in range(1, len(data)):
+        ind_house_left_new = (ind_house_left_last + 1) % count_house
+        cnt_packets_left_new = math.ceil(data[ind_house_left_new][1] / weight_max)
+        ind_house_right_new = (num_house_right_last + 1) % count_house
+        cnt_packets_right_new = math.ceil(data[ind_house_left_new][1] / weight_max)
+        cnt_packets_new = cnt_packets_last - cnt_packets_left_last + cnt_packets_right_new
+        cnt_packets_min = min(cnt_packets_new, cnt_packets_min)
+    return cnt_packets_min
+
+
+fill_tuple()
+print(enum())
 file.close()
